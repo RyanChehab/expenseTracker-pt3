8b85de737a3e7cd90e0b8a0a378d6b10ed2352bc
RyanChehab <ryanchehab20@gmail.com>
2024-11-15 20:45:16 +0200

    jsx <=> updated layout

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,16 @@
 import React from "react"
 
-function navbar(){
+function Navbar(){
     return(
         <nav className="pc flex align-center space-between p-2">
-            <div className="logo mf"></div>
+            <div className="logo mf"><h1>FINANCIAL TRACKER</h1>
+            </div>
+            <div class="Budget flex align-center p-1">
+                <h3 class="mf">Budget:</h3>
+                <p id="budget">$ &nbsp;</p>
+            </div>
         </nav>
     )
 };
+
+export default Navbar;
