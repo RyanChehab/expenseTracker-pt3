93c74c37fc1c129eef897b7dca9260270a88f5bf
RyanChehab <ryanchehab20@gmail.com>
2024-11-15 18:11:02 +0200

    jsx <=> created navbar component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function navbar(){
+    return(
+        <nav className="pc flex align-center space-between p-2">
+            <div className="logo mf"></div>
+        </nav>
+    )
+};
