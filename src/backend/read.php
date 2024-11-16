b08a8662f99fda29ee5166ff5d828bc9fec71d87
RyanChehab <ryanchehab20@gmail.com>
2024-11-17 01:27:42 +0200

    php <=> added headers

--- src/backend/read.php
+++ src/backend/read.php
@@ -1,5 +1,8 @@
 <?php
-session_start();
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
 include 'connection.php';
 $user_id=3;
 
