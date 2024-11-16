41afd93010d257a9c3457ccd45d7fd7554eca916
RyanChehab <ryanchehab20@gmail.com>
2024-11-17 01:05:05 +0200

    jsx <=> created transaction div

--- src/components/Expense_sheet.jsx
+++ src/components/Expense_sheet.jsx
@@ -2,19 +2,52 @@ import React,{useEffect, useState} from "react";
 
 function Expense_sheet(){
     const [expenseData, setExpense] = useState([]);
-    
-    const fetchIncome = ()=> {
-        // fetch income data from api
+
+    const fetchExpense = ()=> {
+        // fetch expense data from api
         fetch('http://localhost/expensetrackerpt3/src/backend/read.php',{
             method: 'POST'
         })
         .then(response => response.json())
         .then(data =>{
-            setIncomeData(data)
+            setExpense(data)
             console.log(data)
-            console.log(incomeData)
+            console.log(setExpense)
             })
         
         .catch(error => console.error("Error fetching data: ", error))
     }
+    // calls the fetchExpense when the component mounts
+    useEffect(() => {
+        fetchExpense(); 
+    }, []);
+
+    
+    if (expenseData.length === 0) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <div>
+            {expenseData.length > 0 ? (
+                expenseData.filter(({type}) => type === "expense")
+                .map(({ id, type, amount, date, notes }) => (
+                    <div key={id} className={`transForm-${type}`}>
+                        <div className="flex space-between m-1">
+                            <p>{type}</p>
+                            <i className="delete fas fa-minus" title="Delete transaction"></i>
+                        </div>
+                        <hr />
+                        <p>Amount: {amount}</p>
+                        <p>Date: {date}</p>
+                        <p>Description: {notes}</p>
+                    </div>
+                ))
+            ) : (
+                <p>No transactions found.</p>
+            )}
+        </div>
+    );
 }
+
+export default Expense_sheet;
