8f6f44805a39a96618d705b2a6b5eba7043383ba
RyanChehab <ryanchehab20@gmail.com>
2024-11-17 00:47:05 +0200

    jsx <=> fetched transactions from api

--- src/components/Expense_sheet.jsx
+++ src/components/Expense_sheet.jsx
@@ -1,1 +1,20 @@
 import React,{useEffect, useState} from "react";
+
+function Expense_sheet(){
+    const [expenseData, setExpense] = useState([]);
+    
+    const fetchIncome = ()=> {
+        // fetch income data from api
+        fetch('http://localhost/expensetrackerpt3/src/backend/read.php',{
+            method: 'POST'
+        })
+        .then(response => response.json())
+        .then(data =>{
+            setIncomeData(data)
+            console.log(data)
+            console.log(incomeData)
+            })
+        
+        .catch(error => console.error("Error fetching data: ", error))
+    }
+}
