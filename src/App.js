5a819898bbdc6b44ff1caeca6d791b50a476b706
RyanChehab <ryanchehab20@gmail.com>
2024-11-16 13:31:58 +0200

    js <=> imported components and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Navbar from "./components/Navbar";
+import Menu from "./components/Menu";
+import Counter from "./components/Test";
 
 function App(){
   return (
+    <>
     <Navbar/ >
+    <Menu />   
+    <Counter /> 
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./css/utilities.css"
+import "./css/nav.css"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
