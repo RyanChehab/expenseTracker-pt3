bceb7273368143a7399bceba5aee8eed99cb0f45
RyanChehab <ryanchehab20@gmail.com>
2024-11-16 17:08:20 +0200

    jsx <=> handeled showing income sheet from the menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React,{useState} from "react";
+import Income_sheet from "./Income_sheet";
 
 function Menu(){
+    const [showIncomeSheet, setShowIncomeSheet] = useState(false);
+
+    const handle = () => {
+        setShowIncomeSheet((show) => !show)
+    }
     return(
         <div className="menu flex space-between align-center">
         <div className="options flex p-2">
-            <button className="p-05 btns" id="income">Income</button>
+            <button className="p-05 btns" id="income" onClick={handle}>Income</button>
+            {showIncomeSheet && <Income_sheet /> }
             <button className="p-05 btns" id="expense">Expenses</button>
         </div>
         <div className="filter">
