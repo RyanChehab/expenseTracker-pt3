93f98c8179c7447177aebcf131d57742ef515732
RyanChehab <ryanchehab20@gmail.com>
2024-11-17 01:34:34 +0200

    jsx <=> debugged

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,8 +5,8 @@ function Navbar(){
         <nav className="pc flex align-center space-between p-2">
             <div className="logo mf"><h1>FINANCIAL TRACKER</h1>
             </div>
-            <div class="Budget flex align-center p-1">
-                <h3 class="mf">Budget:</h3>
+            <div className="Budget flex align-center p-1">
+                <h3 className="mf">Budget:</h3>
                 <p id="budget">$ &nbsp;</p>
             </div>
         </nav>
