f970618ea84a315a573b1437850f32c2ed96e967
RyanChehab <ryanchehab20@gmail.com>
2024-11-17 01:06:14 +0200

    jsx <=> created a useState for displaying the expense div

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,23 +1,33 @@
 import React,{useState} from "react";
 import Income_sheet from "./Income_sheet";
+import Expense_sheet from "./Expense_sheet";
 
 function Menu(){
     const [showIncomeSheet, setShowIncomeSheet] = useState(false);
+    const [showExpenseSheet, setShowExpenseSheet] = useState(false);
 
-    const handle = () => {
+    const handleIncome = () => {
         setShowIncomeSheet((show) => !show)
     }
+
+    const handleExpense = () =>{
+        setShowExpenseSheet((show)=> !show)
+    }
     return(
-        <div className="menu flex space-between align-center">
+    <>
+    <div className="menu flex space-between align-center">
         <div className="options flex p-2">
-            <button className="p-05 btns" id="income" onClick={handle}>Income</button>
-            {showIncomeSheet && <Income_sheet /> }
-            <button className="p-05 btns" id="expense">Expenses</button>
+            <button className="p-05 btns" id="income" onClick={handleIncome}>Income</button>
+            <button className="p-05 btns" id="expense" onClick={handleExpense}>Expenses</button>
         </div>
         <div className="filter">
             <i className="filter fas fa-sliders-h m-2"></i>
         </div>
-     </div>
+    </div>
+    
+    {showIncomeSheet && <Income_sheet /> }
+    {showExpenseSheet && <Expense_sheet />}
+    </>     
     )
 }
 
